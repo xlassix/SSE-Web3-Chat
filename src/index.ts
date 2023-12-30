031373ebef719e7127a3e0cb4a06b8474e3009a0
xlassix <awhy14539@gmail.com>
2023-12-30 19:29:03 +0100

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import http from "http";
+import { start } from './app';
 
-const PORT = process.env.PORT || 3000;
-
-export const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "It Works!",
-    })
-  );
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
-});
+start();
